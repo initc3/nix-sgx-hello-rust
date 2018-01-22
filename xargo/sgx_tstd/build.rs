67912cbb0003723de40ce9d21733b2cb27feba5c
Yu Ding <dingyu02@baidu.com>
2018-01-22 14:20:02 -0800

    v0.9.5 release

--- xargo/sgx_tstd/build.rs
+++ xargo/sgx_tstd/build.rs
@@ -26,50 +26,43 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#![crate_name = "helloworldsampleenclave"]
-#![crate_type = "staticlib"]
+extern crate build_helper;
 
-#![cfg_attr(not(target_env = "sgx"), no_std)]
-#![cfg_attr(target_env = "sgx", feature(rustc_private))]
+use std::env;
+use std::process::Command;
+use build_helper::{run, native_lib_boilerplate};
 
-extern crate sgx_types;
-#[cfg(not(target_env = "sgx"))]
-#[macro_use]
-extern crate sgx_tstd as std;
+fn main() {
+    let target = env::var("TARGET").expect("TARGET was not set");
+    let host = env::var("HOST").expect("HOST was not set");
+    if cfg!(feature = "backtrace") {
+        let _ = build_libbacktrace(&host, &target);
 
-use sgx_types::*;
-use std::string::String;
-use std::vec::Vec;
-use std::io::{self, Write};
-use std::slice;
-
-#[no_mangle]
-pub extern "C" fn say_something(some_string: *const u8, some_len: usize) -> sgx_status_t {
-
-    let str_slice = unsafe { slice::from_raw_parts(some_string, some_len) };
-    let _ = io::stdout().write(str_slice);
-
-    // A sample &'static string
-    let rust_raw_string = "This is a in-Enclave ";
-    // An array
-    let word:[u8;4] = [82, 117, 115, 116];
-    // An vector
-    let word_vec:Vec<u8> = vec![32, 115, 116, 114, 105, 110, 103, 33];
-
-    // Construct a string from &'static string
-    let mut hello_string = String::from(rust_raw_string);
-
-    // Iterate on word array
-    for c in word.iter() {
-        hello_string.push(*c as char);
+        println!("cargo:rustc-cfg=RUST_BACKTRACE=\"1\"");
+        //println!("cargo:rustc-cfg=RUST_BACKTRACE=\"full\"");
     }
+}
 
-    // Rust style convertion
-    hello_string += String::from_utf8(word_vec).expect("Invalid UTF-8")
-                                               .as_str();
+fn build_libbacktrace(host: &str, target: &str) -> Result<(), ()> {
+    let native = native_lib_boilerplate("../libbacktrace", "libbacktrace", "backtrace", ".libs")?;
 
-    // Ocall to normal world for output
-    println!("{}", &hello_string);
-    
-    sgx_status_t::SGX_SUCCESS
+    run(Command::new("sh")
+                .current_dir(&native.out_dir)
+                .arg(native.src_dir.join("configure").to_str().unwrap()
+                                   .replace("C:\\", "/c/")
+                                   .replace("\\", "/"))
+                .arg("--with-pic")
+                .arg("--disable-multilib")
+                .arg("--disable-shared")
+                .arg("--disable-host-shared")
+                .arg(format!("--host={}", build_helper::gnu_target(target)))
+                .arg(format!("--build={}", build_helper::gnu_target(host)))
+                .env("CFLAGS", env::var("CFLAGS").unwrap_or_default() + " -fvisibility=hidden"));
+
+    run(Command::new(build_helper::make(host))
+                .current_dir(&native.out_dir)
+                .arg(format!("INCDIR={}", native.src_dir.display()))
+                .arg("-j").arg(env::var("NUM_JOBS").expect("NUM_JOBS was not set")));
+
+    Ok(())
 }
